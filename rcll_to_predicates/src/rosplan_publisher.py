809b71d8eee2997ab57a338a6949e714f09e6913
Erez Karpas <karpase@gmail.com>
2016-06-21 15:28:12 +0300

    use knowledge update array

--- rcll_to_predicates/src/rosplan_publisher.py
+++ rcll_to_predicates/src/rosplan_publisher.py
@@ -1,10 +1,10 @@
 from common import *
-from rosplan_knowledge_msgs.srv import KnowledgeUpdateService
+from rosplan_knowledge_msgs.srv import KnowledgeUpdateServiceArray
 from rosplan_knowledge_msgs.msg import KnowledgeItem
 from diagnostic_msgs.msg import KeyValue
 
 
-rosplan_knowledge_update_service = 'knowledge_update_service'
+rosplan_knowledge_update_service = 'kcl_rosplan/update_knowledge_base_array'
 
 class ROSPlanPublisher:
 	ADD_KNOWLEDGE = 0
@@ -12,10 +12,11 @@ class ROSPlanPublisher:
 	def __init__(self, service_name = rosplan_knowledge_update_service):
 		self.service_name = service_name		
 		rospy.wait_for_service(service)
-		self.update_knowledge = rospy.ServiceProxy(self.service_name, KnowledgeUpdateService)
+		self.update_knowledge = rospy.ServiceProxy(self.service_name, KnowledgeUpdateServiceArray)
 
 
-	def publish_predicates(self, preds):		
+	def publish_predicates(self, preds):
+		ka = []	
 		for p in preds:
 			k = KnowledgeItem()
 			k.knowledge_type = k.FACT
@@ -25,4 +26,5 @@ class ROSPlanPublisher:
 				kv.key = 'arg_' + str(i)
 				kv.value = p.args[i]
 				k.values.append(kv)
-			self.update_knowledge(self.ADD_KNOWLEDGE, k)
+			ka.append(k)
+		self.update_knowledge(self.ADD_KNOWLEDGE, k)
