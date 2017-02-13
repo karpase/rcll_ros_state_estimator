52355d87c06eed1eaca9036e8314788ad58c1735
Erez Karpas <karpase@gmail.com>
2017-02-13 11:13:35 +0200

    now publish predicated with the correct key

--- rcll_to_predicates/src/rosplan_publisher.py
+++ rcll_to_predicates/src/rosplan_publisher.py
@@ -1,30 +1,39 @@
 from common import *
-from rosplan_knowledge_msgs.srv import KnowledgeUpdateServiceArray
-from rosplan_knowledge_msgs.msg import KnowledgeItem
+from rosplan_knowledge_msgs.srv import KnowledgeUpdateServiceArray, GetDomainAttributeService
+from rosplan_knowledge_msgs.msg import KnowledgeItem, DomainFormula
 from diagnostic_msgs.msg import KeyValue
 
 
 rosplan_knowledge_update_service_array = 'kcl_rosplan/update_knowledge_base_array'
-rosplan_knowledge_update_service = 'kcl_rosplan/update_knowledge_base'
+rosplan_get_domain_predicates_service = '/kcl_rosplan/get_domain_predicates'
 
 class ROSPlanPublisher:
 	ADD_KNOWLEDGE = 0
 
 	def __init__(self):
-		#rospy.wait_for_service(rosplan_knowledge_update_service)
 		rospy.wait_for_service(rosplan_knowledge_update_service_array)
 		self.update_knowledge_array = rospy.ServiceProxy(rosplan_knowledge_update_service_array, KnowledgeUpdateServiceArray)
-		#self.update_knowledge = rospy.ServiceProxy(self.service_name, KnowledgeUpdateService)
+		self.update_predicate_info()
 
-	def publish(self, ka, array):
+	def update_predicate_info(self):
+		self.predicate_key_name = {}
+
+		rospy.wait_for_service(rosplan_get_domain_predicates_service)
+		domain_predicates_service_proxy = rospy.ServiceProxy(rosplan_get_domain_predicates_service, GetDomainAttributeService)
+		pred_info_list = domain_predicates_service_proxy()
+		for pred_info in pred_info_list.items:
+			i = 0
+			for tp in pred_info.typed_parameters:
+				self.predicate_key_name[(pred_info.name, i)] = tp.key
+				print (pred_info.name, i), " = ", tp.key
+				i = i + 1
+
+
+	def publish(self, ka):
 		self.update_knowledge_array(self.ADD_KNOWLEDGE, ka)	
-		#if array:
-		#	self.update_knowledge_array(self.ADD_KNOWLEDGE, ka)	
-		#else:
-		#	for k in ka:
-		#		self.update_knowledge(self.ADD_KNOWLEDGE, k)	
+		
 
-	def publish_predicates(self, preds, array=True):
+	def publish_predicates(self, preds):
 		ka = []	
 		for p in preds:
 			k = KnowledgeItem()
@@ -32,15 +41,15 @@ class ROSPlanPublisher:
 			k.attribute_name = p.head.lower()
 			for i in xrange(len(p.args)):
 				kv = KeyValue()
-				kv.key = 'arg_' + str(i).lower()
+				kv.key = self.predicate_key_name[(k.attribute_name, i)]
 				kv.value = p.args[i].lower()
 				k.values.append(kv)
 			ka.append(k)
-		self.publish(ka, array)
+		self.publish(ka)
 		
 
 
-	def publish_instances(self, object_instances, array=False):
+	def publish_instances(self, object_instances):
 		"""publish instances. input is dictionary where name is key and type is value"""
 		print "adding: ", object_instances
 		ka = []
@@ -51,4 +60,4 @@ class ROSPlanPublisher:
 			k.instance_type = object_instances[obj_name].lower()
 			print k
 			ka.append(k)
-		self.publish(ka, array)
+		self.publish(ka)
