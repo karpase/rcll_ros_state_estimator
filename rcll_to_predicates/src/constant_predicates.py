2cb706401297404b9ac5641e89db1b93c6c011df
Erez Karpas <karpase@gmail.com>
2017-02-13 11:13:58 +0200

    now get team name from parameter

--- rcll_to_predicates/src/constant_predicates.py
+++ rcll_to_predicates/src/constant_predicates.py
@@ -4,9 +4,9 @@ import rospy
 from common import *
 from rosplan_publisher import *
 
-def generate_constant_predicates():	
+def generate_constant_predicates(control_team):	
 	preds = []
-	preds += map(lambda i: Predicate("next",[number_to_object(i), number_to_object(i+1)]), xrange(max_num_materials_for_ring))	
+	#preds += map(lambda i: Predicate("next",[number_to_object(i), number_to_object(i+1)]), xrange(max_num_materials_for_ring))	
 	for team in teams.keys():
 		for i in xrange(1,4):
 			preds.append(Predicate("robotOwner", [robot_to_object(team, i), teams[team]]))
@@ -20,17 +20,18 @@ def generate_constant_predicates():
 		for c in color_dict["base"].keys():
 			magazine = team + "-" + "BSMagazine" + str(c)		
 			preds.append(Predicate("magazineInMachineSide", [magazine, team + "-" + "BS", "OutputSide"]))
-	preds.append(Predicate("control",["CyanTeam"])) # todo: how do we know which team we are?
+	preds.append(Predicate("control",[control_team])) # todo: how do we know which team we are?
 	return preds
 
 
 def main():	
 	rospy.init_node('constant_predicates', anonymous=True)	   
+	team = rospy.get_param("team")
 
 	publisher = ROSPlanPublisher()
 	rate = rospy.Rate(1) # 1hz
 	while not rospy.is_shutdown():
-		preds = generate_constant_predicates()        
+		preds = generate_constant_predicates(team)        
 		publisher.publish_predicates(preds)
 		rate.sleep()
 
